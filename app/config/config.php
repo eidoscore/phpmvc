0a3db5bed37499031543d1a822d1a0a9c374ed01
eidoscore <buatpengaman@yahoo.com>
2020-05-02 06:04:59 +0700

    Menambahkan Database Wrapper dan Detail Mahasiswa

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('BASEURL', 'http://localhost/phpmvc/public');
+
+// koneksi database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
+
+
+?>
--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,50 +1,25 @@
 <?php
 class Mahasiswa_model {
-    // private $mhs = [
-    //     [
-    //         "nama" => "Khaerul Anwar",
-    //         "nim" => "2017102078",
-    //         "email" => "khoerulanwar523@yahoo.com",
-    //         "jurusan" => "Teknik Informatika"
-
-    //     ],
-    //     [
-    //         "nama" => "Ulfiyatul Khotimah",
-    //         "nim" => "2017102068",
-    //         "email" => "ulfiyatul@gmail.com",
-    //         "jurusan" => "Akutansi"
-
-    //     ],
-    //     [
-    //         "nama" => "Try Setia",
-    //         "nim" => "2017102081",
-    //         "email" => "trysn@yahoo.com",
-    //         "jurusan" => "Sistem Informasi"
-
-    //     ]
-    // ];
-
-    // database handler
-    private $dbh;
-    private $stmt;
+    private $table = 'mahasiswa';
+    private $db;
 
     public function __construct()
     {
-        // data source name
-        $dsn = 'mysql:host=localhost;dbname=phpmvc';
-
-        try {
-            $this->dbh = new PDO($dsn, 'root', '');
-        } catch (PDOException $e) {
-            die($e->getMessage());
-        }
+        $this->db = new Database;
     }
 
     public function getAllMahasiswa()
     {
-        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-        $this->stmt->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+        // SELECT * FROM  kasih spasi dikutip akhir
+        $this->db->query('SELECT * FROM ' . $this->table);
+        return $this->db->resultSet();
+    }
+
+    public function getMahasiswaById($id)
+    {
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+        $this->db->bind('id',$id);
+        return $this->db->single();
     }
 }
 
