ccb90873994e720563f0cbdbae3af43cbf5b465b
eidoscore <buatpengaman@yahoo.com>
2020-05-02 01:39:33 +0700

    Menambahkan css dan constant url

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
-    <h1>About Me</h1>
-    <p>Halo Nama Saya <?= $data['nama']; ?>, Pekerjaan <?= $data['pekerjaan']; ?>, Umur <?= $data['umur']; ?> Tahun</p>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/anwar.jpg" alt="Khaerul Anwar" width="250" height="240" class="rounded-circle">
+    <p class="mt-4">Halo Nama Saya <?= $data['nama']; ?>, Pekerjaan <?= $data['pekerjaan']; ?>, Umur <?= $data['umur']; ?> Tahun</p>
+</div>
