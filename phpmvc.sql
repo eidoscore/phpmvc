02f6a6656ee8804b9428fd37eeae88ca064ca3e5
eidoscore <buatpengaman@yahoo.com>
2020-05-03 20:29:05 +0700

    Revisi Update Database

--- phpmvc.sql
+++ phpmvc.sql
@@ -1,25 +1,72 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.1.38-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             10.2.0.5599
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 03, 2020 at 03:27 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Dumping data for table phpmvc.mahasiswa: ~2 rows (approximately)
-/*!40000 ALTER TABLE `mahasiswa` DISABLE KEYS */;
-REPLACE INTO `mahasiswa` (`id`, `nama`, `nim`, `email`, `jurusan`) VALUES
-	(1, 'Khaerul Anwar', '2017102078', 'khoerulanwar523@yahoo.com', 'Teknik Informatika'),
-	(2, 'Ulfiyatul Khotimah', '2017102965', 'ulfiyatul@gmail.com', 'DKV'),
-	(3, 'Try Setia', '2017102081', 'trysn@outlook.com', 'Sistem Informasi'),
-	(5, 'anwar', '1313131', 'buatpengaman@yahoo.com', 'Teknik Informatika');
-/*!40000 ALTER TABLE `mahasiswa` ENABLE KEYS */;
+--
+-- Database: `phpmvc`
+--
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `nim` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `jurusan` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `nim`, `email`, `jurusan`) VALUES
+(1, 'Khaerul Anwar', '2017102078', 'khoerulanwar523@yahoo.com', 'Teknik Informatika'),
+(2, 'Ulfiyatul Khotimah', '2017102965', 'ulfiyatul@gmail.com', 'DKV'),
+(3, 'Try Setia', '2017102081', 'trysn@outlook.com', 'Sistem Informasi'),
+(5, 'anwar', '1313131', 'buatpengaman@yahoo.com', 'Teknik Informatika');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
